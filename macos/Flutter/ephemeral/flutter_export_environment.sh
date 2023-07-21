5c2d5ed06af8b132ec1b04d72d016e759eed093d
Julian KOUNE <jkoune@linagora.com>
2023-07-21 23:39:54 +0200

    chore: upgrade gradle

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\koune\Documents\projects\appstud"
+export "FLUTTER_APPLICATION_PATH=C:\Users\koune\Documents\projects\appstud\cleanarch"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.1"
